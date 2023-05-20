1c85d6cb1cd2907ac3d87e4b6ae9f43e3a6c9c00
AmakoveBridgit <amakovebridgit@gmail.com>
2023-05-20 20:58:06 +0300

    added the for loop that iterates through the list of payments and prints one by one then allows the user to chose the method of payment they want

--- payment.py
+++ payment.py
@@ -2,19 +2,27 @@
 class Payment:
     def __init__(self, payment_methods):
         self.payment_methods = payment_methods
-    def chose_payment_method(self, method):
-     
+    def choose_payment_method(self):
+        print("Available payment methods:")
+        for method in self.payment_methods:
+            print(method)
+        method = input("Enter payment method: ")
         if method in self.payment_methods:
-            print(f"Choose payment method: {method}")
+            print(f"Chosen payment method: {method}")
         else:
-            print(f"{method} is not available. Please choose a different payment method.")
+            print("Error: Invalid payment method.")
     def cancel_payment(self):
         print("Payment transaction cancelled.")
-
-
 payment = Payment(["credit_card", "paypal", "cash"])
 
-payment.chose_payment_method("credit_card")
+payment.choose_payment_method()
 
 payment.cancel_payment()
+
+
+
+
+
+
+
 
