92569e1bb5d7618ac9b09154d172ede1f07f973f
winnieSikinyi <winniesikinyi@gmail.com>
2023-05-20 19:41:22 +0300

    commited changes on class product  initialized a total to zero

--- products.py
+++ products.py
@@ -1,33 +1,32 @@
 class Product:
-    def __init__(self,name,price,description,category):
-        self.name=name
-        self.price=price
-        self.description =description
-        self.category = category
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
 
-    def order(self,items):
-        self.items = items
-        self.total_price = sum([item['product'].price * item['quantity'] for item in items])
-        print(f"Order")
-        
-    def set_category(self, category):
-        self.category = category
-
+    def Order(self, items):
+        total = 0
+        for item in items:
+            total += item['product'].price * item['quantity']
+        print(f"Order Total: Ksh{total}")
 
 items = [{'product': Product('Product1', 10, 'Description1'), 'quantity': 2},
-         {'product': Product('Product2', 20, 'Description2'), 'quantity': 3}]
+         {'product': Product('Product2', 20, 'Description2'), 'quantity': 3},
+         {'product': Product('Product3', 50, 'Description3'), 'quantity': 4},
+         {'product': Product('Product4', 60, 'Description4'), 'quantity': 7},
+         {'product': Product('Product5', 40, 'Description5'), 'quantity': 9}]
 
-product = Product('Product3', 30, 'Description3')
-product.set_category('Category3')
-product.order(items)
+product = Product('Product6', 30, 'Description6')
+product.Order(items)
 
-# >>> from MamaGroceryBackend.product import Product
+
+# >>> from MamaGroceBackend.product import Product
 # >>> order=sum([400*2,300*3])
 # >>> order
 # 1700
 # >>> 
 
-# >>> from MamaGroceryBackend.product import Product
+# >>> from MamaGroceBackend.product import Product
 # >>> product1=("mango",233,"it is sweeter")
 # >>> product1
 # ('mango', 233, 'it is sweeter')
